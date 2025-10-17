44888d37a19c88ca6287064c05eb68977e9ade0e
mahboob alam <mahboob6550@gmail.com>
2025-10-17 23:57:11 +0530

    added cloudinary and multer for saving file temporarly in local storage

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import {v2 as cloudinary } from 'cloudinary'
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET,
+    secure:true,
+})
+
+const uploadOnCloudinary = async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null;
+        const result =await cloudinary.uploader.upload(localFilePath,{
+            resource_type:'auto',
+        })
+        console.log("file uploaded successfully",result.url);
+        return result;
+
+    }catch(error){
+        fs.unlinkSync(localFilePath);
+        //   this will remove the locally saved temporary file if upload operation
+        //          got failed.
+    }
+}
+
+
+export {uploadOnCloudinary};
+//      then we have to import like
+//      import {uploadOnCloudinary} from cloudinary.js like this
+//  
+//    but we can use export default also
