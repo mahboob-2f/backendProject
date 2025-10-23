49f756af9230ebdfcb58d7637926ac8a63884fcb
mahboob alam <mahboob6550@gmail.com>
2025-10-23 18:24:43 +0530

    adding method to give unique name to files that are to be uploaded

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,12 +1,15 @@
 import multer from "multer";
-
+import crypto from 'crypto'
+import path from 'path'
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp")
   },
   filename: function (req, file, cb) {
-     
+    // fn =crypto.randomBytes(8).toString("hex")+path.extname(file.originalname);
+    //     we can do above for creating unique file name  
+    //
     cb(null, file.originalname)
   }
 })
