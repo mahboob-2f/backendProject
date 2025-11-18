a9d5ec7d69914367137d80f4dedc6438d56b0519
mahboob alam <mahboob6550@gmail.com>
2025-11-19 00:48:32 +0530

    writing aggregation pipelines ( match,lookup,addFields,project)

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -477,7 +477,78 @@ export const updateUserCoverImage= asyncHandler(async(req,res)=>{
 })
 
 export const getUserChannelProfile= asyncHandler(async(req,res)=>{
-    
+    const {username} = req.params;
+
+    if(!username?.trim()){
+        throw new ApiError(400,"username is missing");
+    }
+
+    const channel= await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase(),
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo",
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in : [req.user?._id,"$subscribers.subscriber"]},
+                        then: true,
+                        else:false
+                    }
+                }
+            }
+        },
+        {
+            $project:{
+                fullname:1,
+                subscribersCount:1,
+                channelSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1,
+                username:1,
+            }
+        }
+        
+    ])
+
+    console.log("channel : ",channel);
+
+    if(!channel?.length){
+        throw new ApiError(404,'Channel does not exist ');
+    }
+
+    return res.status(200)
+        .json(
+            new ApiResponse(200,channel?.[0],"User channel fetched successfully")
+        )
+
+
 })
 
 
